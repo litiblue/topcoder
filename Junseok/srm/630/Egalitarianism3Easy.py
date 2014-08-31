5ba8d25aab222dc9df9689ed9b4334811032e536
dev <dev@ANS01.(none)>
2014-08-31 12:39:50 +0900

    ..

--- Junseok/srm/630/Egalitarianism3Easy.py
+++ Junseok/srm/630/Egalitarianism3Easy.py
@@ -1,14 +1,20 @@
 import sys
+import pprint
 from collections import defaultdict
 
 class Egalitarianism3Easy(object):
 	
 	def maxCities(self, n, a, b, len):
-		d = [[sys.maxint] * (n+1)] * (n+1)
+		d = defaultdict(defaultdict)
+		for i in range(0,n+1):
+			for j in range(0,n+1):
+				d[i][j] = sys.maxint
+		for i in range(1,n+1):
+			d[i][j] = 0
 
 		for i in range(n-1):
 			d[a[i]][b[i]] = d[b[i]][a[i]] = len[i]
-
+		
 		for k in range(1,n+1):
 			for i in range(1,n+1):
 				for j in range(1,n+1):
@@ -26,10 +32,10 @@ class Egalitarianism3Easy(object):
 		return max_cnt
 
 if __name__ == '__main__':
-	egal = Egaliterianism3Easy()	
-	#print egal.maxCities(4, [1,1,1], [2,3,4], [1,1,1])
+	egal = Egalitarianism3Easy()	
+	print egal.maxCities(4, [1,1,1], [2,3,4], [1,1,1])
 	#print egal.maxCities(6, [1,2,3,2,3], [2,3,4,5,6], [2,1,3,2,3])
 	#print egal.maxCities(10, [1,1,1,1,1,1,1,1,1,1], [2,3,4,5,6,7,8,9,10], \
 	#[1000,1000,1000,1000,1000,1000,1000,1000,1000])
 	#print egal.maxCities(2, [1], [2], [3])
-	print egal.maxCities(1, [], [], [])
+	#print egal.maxCities(1, [], [], [])
